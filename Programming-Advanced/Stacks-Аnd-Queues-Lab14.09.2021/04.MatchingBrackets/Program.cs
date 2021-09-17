4688c9eb1a00c485763b97a33e1ad646a93e4f0a
IvanGeorgiev2108 <ivan.georgiev2108@gmail.com>
2021-09-17 13:19:51 +0300

    Added Programming Advanced -> Stacks and Queues Lab Exercise!

---
+++ Programming-Advanced/Stacks-Аnd-Queues-Lab14.09.2021/04.MatchingBrackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> stackIndexes = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stackIndexes.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int index = stackIndexes.Pop();
+                    string result = input.Substring(index, i - index + 1);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Advanced/Stacks-Аnd-Queues-Lab14.09.2021/08.TrafficJam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfCarsToPassOnGreenLight = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            int passedCars = 0;
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                else if (line == "green")
+                {
+                    for (int i = 0; i < numbersOfCarsToPassOnGreenLight; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            var car = cars.Dequeue();
+                            Console.WriteLine(car + " passed!");
+                            passedCars++;
+                        }
+                    }
+                }
+                else 
+                {
+                    cars.Enqueue(line);
+                }
+            }
+            Console.WriteLine(passedCars + " cars passed the crossroads.");
+        }
+    }
+}
